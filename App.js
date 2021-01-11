681374ccd24ce90662c463683e881ee1ccb4bb9c
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-12 02:19:03 +0900

    Create axios

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
+// https://api.openweathermap.org/data/2.5/weather?lat=37.367717500731544&lon=126.93501768887215&appid=1cce20274897538f27f72a370b9bb045units=metric
 import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
 
-const API_KEY = "d65dac0a0139c95b2362e104cecfd7a6";
+const API_KEY = "1cce20274897538f27f72a370b9bb045";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log(data);
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
-        coords: { longitude, latitude }, //위도와, 경도의 값을 가지고옴
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false }); //위도와 경도의 값을 가지고왔을때에는 isloading 을 "false"로 설정
-      //send API
+
+      this.getWeather(latitude, longitude); //latitude와 longitude를 가지고온다.
+      this.setState({ isLoading: false });
     } catch (error) {
-      Alert.alert("cant find you");
+      Alert.alert("Can't find you.", "So sad");
     }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state; //상태값을 다시정의해준다.
+    const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
   }
 }
